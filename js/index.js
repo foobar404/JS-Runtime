0ce38e51db310af01dd9a14c4ede4eed2c942e41
unknown <nicholas.gebhart@gmail.com>
2019-08-10 14:37:49 -0700

    commit

--- js/index.js
+++ js/index.js
@@ -6,18 +6,10 @@ document.querySelector("#run").onclick = runAndMeasure(e);
 
 function runAndMeasure() {
   //performance.now
-  // let start = performance.now()
-
-  // // Code goes here
-
-  // let end = performance.now();
-  // return `The script took ${start - end} miliseconds.`
-
-  console.time();
-  for (i = 0; i < 100000; i++) {
-    // some code
-  }
-  console.timeEnd();
+  let start = performance.now();
 
   // Code goes here
+
+  let end = performance.now();
+  return `The script took ${start - end} miliseconds.`;
 }
