4ef378f5adc229f55b3df5e33ea301939db7d386
unknown <nicholas.gebhart@gmail.com>
2019-08-10 14:57:28 -0700

    working prototype

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
+var output = document.querySelector("#output");
 document.querySelector("#save").onclick = e => {
   alert("click");
 };
 
-document.querySelector("#run").onclick = runAndMeasure(e);
+document.querySelector("#run").onclick = e => {
+  document.querySelector("#output").innerHTML = runAndMeasure(e);
+};
 
 function runAndMeasure() {
+  var code = document.querySelector("#sandbox").value;
+
   //performance.now
   let start = performance.now();
 
-  // Code goes here
+  try {
+    eval(code);
+  } catch (err) {
+    output.innerHTML = err;
+  }
 
   let end = performance.now();
-  return `The script took ${start - end} miliseconds.`;
+  return `The script took ${end - start} miliseconds.`;
 }
+
+// function fib(n) {
+//     let result;
+//     if (n === 1 || n === 2) {
+//       result = 1;
+//     } else {
+//       result = fib(n - 1) + fib(n - 2);
+//     }
+//     return result;
+//   }
+
+//   console.log(fib(35));
