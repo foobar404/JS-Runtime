d4070e63c320f28d32d4483cd0e06fd738b942b7
unknown <nicholas.gebhart@gmail.com>
2019-08-10 14:34:04 -0700

    added about, small changes to css, started js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-// index.js
+document.querySelector("#save").onclick = e => {
+  alert("click");
+};
+
+document.querySelector("#run").onclick = runAndMeasure(e);
+
+function runAndMeasure() {
+  //performance.now
+  // let start = performance.now()
+
+  // // Code goes here
+
+  // let end = performance.now();
+  // return `The script took ${start - end} miliseconds.`
+
+  console.time();
+  for (i = 0; i < 100000; i++) {
+    // some code
+  }
+  console.timeEnd();
+
+  // Code goes here
+}
